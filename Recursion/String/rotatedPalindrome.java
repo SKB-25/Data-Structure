ee10b68a804719dce87f2fa54cf6c0ae24c8554a
SACHIN KUMAR BAGHEL <sachin8750546601baghel@gmail.com>
2022-07-03 14:28:15 +0530

    Updated

--- Recursion/String/rotatedPalindrome.java
+++ Recursion/String/rotatedPalindrome.java
@@ -21,7 +21,23 @@ public class rotatedPalindrome {
     private static boolean isPalinrome(String str , int count) {
         
         if(str.length() == 0){
+            return true;
+        }
+        if( count == 0){
             return false;
+        }
+
+        if(isPalinromeHelper(str)){
+            return true;
+        }
+        
+        return isPalinrome(str.substring(1) + str.charAt(0) , count - 1);
+
+    }
+   
+    private static boolean isPalinromeHelper(String str) {
+        if(str.length() == 0){
+            return true;
         }
 
         String reverse = "";
@@ -31,31 +47,11 @@ public class rotatedPalindrome {
         }
        
 
-        if(reverse == str){
+        if(reverse.equals(str)){
             return true;
         }
-        
-        return isPalinrome(str.substring(1) + str.charAt(0) , count - 1);
-
+        else{
+            return false;
+        }
     }
-   
-    // private static boolean isPalinromeHelper(String str) {
-    //     if(str.length() == 0){
-    //         return true;
-    //     }
-
-    //     String reverse = "";
-
-    //     for(int i = 0; i<str.length(); i++){
-    //          reverse = str.charAt(i) + reverse;
-    //     }
-       
-
-    //     if(reverse == str){
-    //         return true;
-    //     }
-    //     else{
-    //         return false;
-    //     }
-    // }
 }
